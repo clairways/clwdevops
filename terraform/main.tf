86e92f464ac9374abbf12c1dbfecbaace431e88d
Ricardo Serrano-Smith <rsmith9945@gmail.com>
2024-02-20 11:05:45 -0700

    chore: added terraform config for updating lambda layer

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket = "clairways-terraform-states"
+    key    = "dev/connect/clwdevops-lambda-layer/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {}
+
+module "generate_clwdevops_lambda_layer" {
+  source       = "git@github.com:clairways/terraform-modules.git//lambda_layer?ref=v1.7.1"
+  name         = "clwdevops-lambda-layer"
+  runtime      = "python3.10"
+  mount_point  = "../"
+  requirements = "../requirements.txt"
+  out_dir      = "../output"
+}
